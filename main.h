53fc426513b2e06a001c9de59cae394fd10d3c6f
root <you@example.com>
2023-04-26 06:49:52 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <signal.h>
+
+char *get_env(char *command);
+int exec(char **args);
+char *check_path(char *command);
+char *f_read(void);
+char **tokenize(char *line);
+void _signal(int sig);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+int _putchar(char c);
+
+#endif
